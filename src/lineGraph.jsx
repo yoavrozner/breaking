d4d43db2a108ec87a8a7c1b5173a4c2e8d50c745
ronnie-antebi <rontebi@gmail.com>
2022-06-21 18:57:02 +0300

    clean comments

--- src/lineGraph.jsx
+++ src/lineGraph.jsx
@@ -3,25 +3,21 @@ import { ResponsiveLine } from 'nivo';
 import data from "./data.json"
 
 
-// const ShoverTooltip = (datapoint) => {
-//     console.log(datapoint)
-//     return <div>TEST</div>
-// }
-
-const split_anomaly_data = (data) => {
+const color_anomaly_data = (data) => {
     const anomalies = []
     const regulars = []
     data[0].data.forEach((point) => {
         if (point.is_annomaly > 0.5) {
             anomalies.push(point)
             regulars.push({...point, 'y': null})
-        }
+            }
         else {
             anomalies.push({...point, 'y': null})
             regulars.push(point)
         }
     })
     return [
+        data[0],
         {
             "id": data[0].id + " anomalies",
             "data": anomalies
@@ -33,23 +29,10 @@ const split_anomaly_data = (data) => {
     ]
 }
 
-// const color_anomalies = (data) => {
-//     return data[0].data.forEach((point) => {
-//         if (point.is_annomaly > 0.5) {
-//             point['color'] = 'red'
-//         }
-//         else {
-//             point['color'] = 'green'
-//         }
-//     }
-//     )
-// }
-
 const Graph = () => {
     return (
         <ResponsiveLine
-            data={split_anomaly_data(data)}
-            // data={color_anomalies}
+            data={color_anomaly_data(data)}
             margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
             xScale={{
                 type: 'time',
@@ -63,19 +46,7 @@ const Graph = () => {
             }}
             enableSlices={false}
             crosshairType="cross"
-            curve={'monotoneX'}
-            colors={['red', 'green']}
-            // colors={(point) => {
-            //     console.log(point)
-            //     // data.forEach(({}))
-            //     let is_anomaly = point.is_annomaly > 0.5
-            //     console.log(is_anomaly)
-            //     return is_anomaly ? 'red' : 'green'
-            // }
-            // }
-            // colorBy={'color'}
-            pointSize={16}
-            pointBorderWidth={1}
+            colors={['lime', 'red', 'lime']}
             useMesh={true}
         />
     )
