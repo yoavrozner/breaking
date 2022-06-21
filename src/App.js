12190a23a8f864b50a3c1dd2323511a8f94c68eb
yoavrozner <rozner.yoav@gmail.com>
2022-06-21 12:38:30 +0300

    feat: sidenav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Graph from './lineGraph.jsx';
+import Side from './components/Sidenav.jsx';
 // import Graph from './line.jsx';
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
@@ -21,17 +22,20 @@ function App() {
         </Container>
       </Navbar>
       <p>שלום לכולם</p>
+      <Side className="side" />
 
-      <div className='card'>
-        <div className='container'>
-          <h4><b>חתמצ מספר 1</b></h4>
-        </div>
+      <div className='contentContainer'>
+        <div className='card'>
+          <div className='container'>
+            <h4><b>חתמצ מספר 1</b></h4>
+          </div>
 
-        <div className='graphWrapper'>
-          <Graph />
-          <div className='desc'>
-            <p>תיאור אתר מספר 1</p>
-            <img src={require('./seal.png')} alt="" className='imageSeal' />
+          <div className='graphWrapper'>
+            <Graph />
+            <div className='desc'>
+              <p>תיאור אתר מספר 1</p>
+              <img src={require('./seal.png')} alt="" className='imageSeal' />
+            </div>
           </div>
         </div>
       </div>
